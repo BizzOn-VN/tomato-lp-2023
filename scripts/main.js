55b8531f12c8169e3e151aa925176759cf3e0da7
chikien111 <38467294+chikien111@users.noreply.github.com>
2025-12-24 10:25:54 +0700

    update page

--- scripts/main.js
+++ scripts/main.js
@@ -33,6 +33,27 @@ $(".md-section-5").click(function() {
         'slow');
 });
 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
 $(window).scroll(function(){
     if ($(window).scrollTop() >= 5) {
         $('.page-header .container-header').addClass('scroll-top');
